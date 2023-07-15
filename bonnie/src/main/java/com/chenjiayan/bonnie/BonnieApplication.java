1344ca69ed0deb2046e62d186abf5d7fca3fcf31
zhangliqian <13124847347@163.com>
2023-07-15 12:36:09 +0800

    增加模块

---
+++ bonnie/src/main/java/com/chenjiayan/bonnie/BonnieApplication.java
@@ -0,0 +1,13 @@
+package com.chenjiayan.bonnie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BonnieApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BonnieApplication.class, args);
+    }
+
+}
---
+++ bonnie/src/test/java/com/chenjiayan/bonnie/BonnieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chenjiayan.bonnie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BonnieApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
