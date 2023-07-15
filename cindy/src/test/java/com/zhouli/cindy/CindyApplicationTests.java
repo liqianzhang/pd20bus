6ea9298c7e817bc39bc8d0ad6f45cdff4085ce6b
zhangliqian <13124847347@163.com>
2023-07-15 12:36:52 +0800

    增加4个模块

---
+++ cindy/src/test/java/com/zhouli/cindy/CindyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zhouli.cindy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CindyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
